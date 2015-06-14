01990ef6cab330d2be7fa09c02d33901c9e27ba8
naddy095 <nadeemmohammad9@gmail.com>
2015-06-14 19:48:35 +0530

    CBSER structure commited

---
+++ app/src/main/java/com/example/componentservices/ComponentBaseServices.java
@@ -0,0 +1,8 @@
+package com.example.componentservices;
+
+/**
+ * Created by nadeem on 14-06-2015.
+ * All the data related to user device goes in here. eg - IMEI number etc.
+ */
+public class ComponentBaseServices {
+}
