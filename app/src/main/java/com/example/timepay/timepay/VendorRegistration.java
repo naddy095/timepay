71edaf6e7fe56d00dc98d6d70dbe0e5b5dc38d9c
pawan baranwal <pawankumarbaranwal@gmail.com>
2015-06-19 00:27:53 +0530

    Take Image from Gallery or Camera

--- app/src/main/java/com/example/timepay/timepay/VendorRegistration.java
+++ app/src/main/java/com/example/timepay/timepay/VendorRegistration.java
@@ -1,15 +1,36 @@
 package com.example.timepay.timepay;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
 
 
-public class VendorRegistration extends ActionBarActivity {
+public class VendorRegistration extends ActionBarActivity implements View.OnClickListener {
 
+
+    private static final int CAPTURE_IMAGE_FROM_CAMERA = 0;
+    private static final int LOAD_IMAGE_FROM_GALLERY=1;
     EditText expiryDate;
+    EditText panNo;
+    Button uploadPAN ;
+    ImageView imageOfPANCard;
+    Intent builderIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +38,17 @@ public class VendorRegistration extends ActionBarActivity {
 
 
         initialize();
+        Log.i("VendorRegistration", "beforeListener");
+        uploadPAN.setOnClickListener(this);
+        Log.i("VendorRegistration", "afterListener");
+
     }
 
     private void initialize() {
         expiryDate = (EditText)findViewById(R.id.etExpiryDate);
+        uploadPAN=(Button)findViewById(R.id.btnUploadPAN);
+        panNo=(EditText)findViewById(R.id.etPANNumber);
+        imageOfPANCard=(ImageView)findViewById(R.id.ivPANImage);
     }
 
 
@@ -44,5 +72,62 @@ public class VendorRegistration extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view ==uploadPAN){
+            Log.i("VendorRegistration","onclick");
+            final CharSequence[] uploadPanOptions={"Take a Picture","Choose From Gallery"};
+            AlertDialog.Builder builder=new AlertDialog.Builder(VendorRegistration.this);
+
+                builder.setTitle("Choose Options");
+                builder.setItems(uploadPanOptions, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (uploadPanOptions[i].equals("Take a Picture")){
+                            builderIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(builderIntent, CAPTURE_IMAGE_FROM_CAMERA);
+
+                        }else if (uploadPanOptions[i].equals("Choose From Gallery")){
+                            builderIntent=new Intent (Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(builderIntent, LOAD_IMAGE_FROM_GALLERY);
+                        }
+                    }
+                });
+            builder.setInverseBackgroundForced(true);
+            builder.create();
+            builder.show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap bitmap ;
+        Bitmap resizedBitmap;
+            if (requestCode==LOAD_IMAGE_FROM_GALLERY && resultCode==RESULT_OK && data !=null){
+                Uri selectedImageFromUri=data.getData();
+                String pathOFImage=getRealPathFromURI(selectedImageFromUri);
+                File imgFile=new File(pathOFImage);
+                bitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                resizedBitmap = Bitmap.createScaledBitmap(bitmap, 80, 45, false);
+                imageOfPANCard.setImageBitmap(resizedBitmap);
+            }else if (requestCode==CAPTURE_IMAGE_FROM_CAMERA && resultCode ==RESULT_OK){
+                Bundle extras = data.getExtras();
+                bitmap = (Bitmap) extras.get("data");
+                resizedBitmap = Bitmap.createScaledBitmap(bitmap, 80, 45, false);
+                imageOfPANCard.setImageBitmap(resizedBitmap);
+                }
+    }
+
+    private String getRealPathFromURI(Uri selectedImageFromUri) {
+        Cursor cursor = getContentResolver().query(selectedImageFromUri, null, null, null, null);
+        if (cursor == null) {
+            return selectedImageFromUri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
     }
 }
